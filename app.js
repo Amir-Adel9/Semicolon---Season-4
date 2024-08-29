9e9f8654e575f6ebca8cacefc6205e0052730fe8
Amir-Adel9 <amiradel7152@gmail.com>
2024-08-29 18:24:30 +0300

    Session 3 work

---
+++ app.js
@@ -0,0 +1,35 @@
+import { sayHello } from './globals.js';
+import { PI } from './globals.js';
+
+sayHello('Amir');
+
+// function calculateArea(radius) {
+//   return PI * radius * radius;
+// }
+
+// console.log('Area of a circle with radius 3: ', calculateArea(3));
+
+// function getAsyncData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('Data received from the server');
+//     }, 2000);
+//   });
+// }
+
+// getAsyncData().then((data) => {
+//   console.log(data);
+// });
+
+// async function getAsyncDataNew() {
+//   const data = await new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('Data received from the server');
+//     }, 2000);
+//   });
+//   return data;
+// }
+
+// getAsyncDataNew().then((data) => {
+//   console.log(data);
+// });
---
+++ globals.js
@@ -0,0 +1,5 @@
+export function sayHello(name) {
+  console.log('Hello ', name);
+}
+
+export const PI = 3.14159265359;
